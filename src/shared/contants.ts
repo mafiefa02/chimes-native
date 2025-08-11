f180e4119b59b074298190e5b10aa1fc21747d50
mafiefa02 <91366510+mafiefa02@users.noreply.github.com>
2025-08-11 21:09:29 +0700

    chore: remove unused `ENTRY_COMPONENT_ID` constant

--- src/shared/contants.ts
+++ src/shared/contants.ts
@@ -1,6 +1,5 @@
 import pkg from '../../package.json';
 import { capitalize } from './utils';
 
-export const ENTRY_COMPONENT_ID = 'root';
 export const APP_NAME = capitalize(pkg.name);
 export const APP_ID = pkg.config.appId;
