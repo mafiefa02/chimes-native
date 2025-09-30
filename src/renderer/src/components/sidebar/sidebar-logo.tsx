c13afaa49e719c939e363a4c7c6d4a44608a407c
mafiefa02 <91366510+mafiefa02@users.noreply.github.com>
2025-09-30 11:26:40 +0700

    chore: formatting

--- src/renderer/src/components/sidebar/sidebar-logo.tsx
+++ src/renderer/src/components/sidebar/sidebar-logo.tsx
@@ -1,6 +1,6 @@
+import { breadcrumbItem } from '../../lib/animations';
 import { useSidebar } from './hooks/use-sidebar';
 import { motion, AnimatePresence } from 'motion/react';
-import { breadcrumbItem } from '../../lib/animations';
 
 export const SidebarLogo = () => {
   const { isSidebarOpen } = useSidebar();
