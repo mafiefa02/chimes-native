b0a55711ea7a0014259ddc210d6a17cb09c3443f
mafiefa02 <91366510+mafiefa02@users.noreply.github.com>
2025-10-07 17:46:46 +0700

    refactor: change `useAppConfig` hook structure

--- src/renderer/src/hooks/use-app-config.ts
+++ src/renderer/src/hooks/use-app-config.ts
@@ -1,17 +1,13 @@
 import { AppConfig } from '../../../shared/types';
 import { useState, useEffect } from 'react';
 
-export const useAppConfig = <K extends keyof AppConfig>(key: K) => {
+export const useAppConfig = (key: keyof AppConfig) => {
   const [value, setValue] = useState(window.services.appConfig.get(key));
 
   useEffect(() => {
-    const unsubscribe = window.services.appConfig.onChange(key, (newValue) => {
-      setValue(newValue);
-    });
-
-    return () => {
-      unsubscribe();
-    };
+    const setAppConfig = (newValue: string) => setValue(newValue);
+    const unsubscribe = window.services.appConfig.onChange(key, setAppConfig);
+    return () => unsubscribe();
   }, [key]);
 
   return value;
