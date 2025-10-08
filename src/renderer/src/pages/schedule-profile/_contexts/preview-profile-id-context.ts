79b7610c4a8ce1862988339bac887c80044128fd
mafiefa02 <91366510+mafiefa02@users.noreply.github.com>
2025-10-08 18:42:23 +0700

    chore: remove unused `PreviewProfileIdContext`

--- src/renderer/src/pages/schedule-profile/_contexts/preview-profile-id-context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext, Dispatch, SetStateAction } from 'react';
-
-type PreviewProfileIdContextType = {
-  previewProfileId: string;
-  setPreviewProfileId: Dispatch<SetStateAction<string>>;
-};
-
-export const PreviewProfileIdContext =
-  createContext<PreviewProfileIdContextType | null>(null);
--- src/renderer/src/pages/schedule-profile/index.tsx
+++ src/renderer/src/pages/schedule-profile/index.tsx
@@ -10,39 +10,36 @@ import {
 } from '../../components/ui/resizable';
 import { ProfileSelector } from './_components/profile-selector';
 import { WeeklySchedule } from './_components/weekly-schedule';
-import { PreviewProfileIdProvider } from './_contexts/preview-profile-id-provider';
 
 export const ScheduleProfilePage = () => {
   return (
-    <PreviewProfileIdProvider>
-      <PageLayout>
-        <PageHeader>
-          <PageTitle>
-            <PageTitleTitle>Schedule Profile</PageTitleTitle>
-            <PageTitleDescription>
-              Create a schedule to be applied weekly
-            </PageTitleDescription>
-          </PageTitle>
-        </PageHeader>
-        <ResizablePanelGroup
-          className="size-full"
-          direction="horizontal"
+    <PageLayout>
+      <PageHeader>
+        <PageTitle>
+          <PageTitleTitle>Schedule Profile</PageTitleTitle>
+          <PageTitleDescription>
+            Create a schedule to be applied weekly
+          </PageTitleDescription>
+        </PageTitle>
+      </PageHeader>
+      <ResizablePanelGroup
+        className="size-full"
+        direction="horizontal"
+      >
+        <ResizablePanel minSize={60}>
+          <WeeklySchedule />
+        </ResizablePanel>
+        <ResizableHandle
+          withHandle
+          className="mx-3"
+        />
+        <ResizablePanel
+          defaultSize={25}
+          minSize={25}
         >
-          <ResizablePanel minSize={60}>
-            <WeeklySchedule />
-          </ResizablePanel>
-          <ResizableHandle
-            withHandle
-            className="mx-3"
-          />
-          <ResizablePanel
-            defaultSize={25}
-            minSize={25}
-          >
-            <ProfileSelector />
-          </ResizablePanel>
-        </ResizablePanelGroup>
-      </PageLayout>
-    </PreviewProfileIdProvider>
+          <ProfileSelector />
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </PageLayout>
   );
 };
