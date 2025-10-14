daf7a251519f2b33888a1b8d917bd3c8817d6d32
mafiefa02 <91366510+mafiefa02@users.noreply.github.com>
2025-10-14 12:09:33 +0700

    fix: remove `/sounds` and `/settings` from sidebar

--- src/renderer/src/lib/constants.ts
+++ src/renderer/src/lib/constants.ts
@@ -1,11 +1,11 @@
 import { RouteNode } from './types';
-import { CalendarIcon, HomeIcon, MusicIcon, SettingsIcon } from 'lucide-react';
+import { CalendarIcon, HomeIcon } from 'lucide-react';
 
 export const ISO_DAY_OF_WEEKS = Array.from({ length: 7 }, (_, i) => i + 1);
 export const IS_SERVER = typeof window === 'undefined';
 export const parentRoutes: RouteNode[] = [
   { icon: HomeIcon, href: '/', label: 'Daily Schedule' },
   { icon: CalendarIcon, href: '/schedule-profile', label: 'Schedule Profile' },
-  { icon: MusicIcon, href: '/sounds', label: 'Sounds' },
-  { icon: SettingsIcon, href: '/settings', label: 'Settings' },
+  // { icon: MusicIcon, href: '/sounds', label: 'Sounds' },
+  // { icon: SettingsIcon, href: '/settings', label: 'Settings' },
 ];
